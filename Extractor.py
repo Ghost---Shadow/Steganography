d1eea25bc49ca2ed092bfb0a5b5a1fe41834f6d9
Souradeep-Nanda <souradeep_nanda@hotmail.com>
2016-07-04 08:25:23 +0530

    Implemented majority voting system

--- Extractor.py
+++ Extractor.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import random
+from collections import Counter
 
 # Level of redundancy
 NUMBER_OF_PASSES = 8
@@ -15,17 +16,20 @@ bits = (1<<shift)-1
 # Fingerprint dimension
 N = 128
 
+# Save the votes
+votes = [[[] for i in range(N)] for j in range(N)]
+
 # Load the image
 hiddenFile = Image.open("./output.png")
 hiddenPix = hiddenFile.load()
 extractedFile = Image.new("RGB",(N,N),"white")
 extractedPix = extractedFile.load()
 
-# Hide data in LSB of a channel (RGBA)
+# Load data hidden in LSB of a channel (RGBA)
 def getData(x,y,channel):
     rgba = list(hiddenPix[x,y])
     val = (rgba[channel] & bits) << (8-shift)
-    return (val,)*4
+    return val
 
 # One iteration of the stenography
 def runPass(channel):
@@ -33,11 +37,23 @@ def runPass(channel):
         for j in range(N):
             x = random.randint(0,hiddenFile.size[0]-1)
             y = random.randint(0,hiddenFile.size[1]-1)
-            extractedPix[i,j] = getData(x,y,channel)
+            #extractedPix[i,j] = getData(x,y,channel)
+            votes[i][j].append(getData(x,y,channel))      
 
 # Run multiple passes
 for i in range(NUMBER_OF_PASSES):
     runPass(i%4)
+
+# Resolve a vote
+def majorityVote(v):
+    v = Counter(v)
+    return v.most_common(1)[0][0]
+
+# Resolve all votes
+for i in range(N):
+    for j in range(N):
+        pixel = majorityVote(votes[i][j])
+        extractedPix[i,j] = (pixel,)*3
 
 # Close files
 hiddenFile.close()
